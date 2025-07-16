ed9469ac16a166b26734c5efb4c899b4feaa98a4
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-07-16 17:40:17 +0200

    ajout doc swagger

--- app/router.js
+++ app/router.js
@@ -10,17 +10,152 @@ const router = express.Router();
 // Routes : --------------------------------------------------------------- //
 
 // Route pour récupérer toutes les blagues : 
-router.get("/api/v1/jokes", BlagueController.getAllJokes ); 
+/**
+* @swagger
+ * /api/v1/jokes:
+ *   get:
+ *     summary: Récupère toutes les blagues
+ *     description: Retourne toutes les blagues enregistrées dans la base de données
+ *     tags:
+ *       - Blagues
+ *     responses:
+ *       200:
+ *         description: Succès - retour de toutes les blagues
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   question:
+ *                     type: string
+ *                     example: "Quelle est la femelle du hamster ?"
+ *                   answer:
+ *                     type: string
+ *                     example: "L'Amsterdam"
+ *       404:
+ *         description: Aucune blague trouvée
+ *       500:
+ *         description: Erreur interne du serveur
+ */
+router.get("/api/v1/jokes", BlagueController.getAllJokes);
 
-// Route pour récupérer une seule blague de manière aléatoire : 
-router.get("/api/v1/jokes/random", BlagueController.getOneJokeRandomly); 
+
+
+// Route pour récupérer une seule blague de manière aléatoire :
+/**
+ * @swagger
+ * /api/v1/jokes/random:
+ *   get:
+ *     summary: Récupère une blague de manière aléatoire
+ *     tags:
+ *       - Blagues
+ *     responses:
+ *       "200":
+ *         description: Succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 question:
+ *                   type: string
+ *                   example: "Quelle est la femelle du hamster ?"
+ *                 answer:
+ *                   type: string
+ *                   example: "L'Amsterdam"
+ *       "404":
+ *         description: Aucune blague n'a été trouvée
+ *       "500":
+ *         description: Erreur interne
+ */
+router.get("/api/v1/jokes/random", BlagueController.getOneJokeRandomly);
+
+
 
 // Route pour récupérer une seule blague par ID : 
+/**
+ * @swagger
+ * /api/v1/jokes/:id:
+ *   get:
+ *     summary: Récupère une blague par son ID (identifiant)
+ *     tags:
+ *       - Blagues
+ *     responses:
+ *       "200":
+ *         description: Succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 question:
+ *                   type: string
+ *                   example: "Quelle est la femelle du hamster ?"
+ *                 answer:
+ *                   type: string
+ *                   example: "L'Amsterdam"
+ *       "404":
+ *         description: Aucune blague n'a été trouvée
+ *       "500":
+ *         description: Erreur interne
+ */
 router.get("/api/v1/jokes/:id", BlagueController.getOneJoke);
 
-// Route pour ajouter une nouvelle blague en BDD:
-router.post("/api/v1/jokes", BlagueController.createNewJoke);
 
+
+// Route pour ajouter une nouvelle blague en BDD:
+/**
+ * @swagger
+ * /api/v1/jokes:
+ *   post:
+ *     summary: Rajoute une blague en base de données
+ *     tags:
+ *       - Blagues
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               question:
+ *                 type: string
+ *                 example: "Quel est le sport préféré des insectes ?"
+ *               answer:
+ *                 type: string
+ *                 example: "Le cricket"
+ *     responses:
+ *       "200":
+ *         description: Blague ajoutée avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 42
+ *                 question:
+ *                   type: string
+ *                   example: "Quel est le sport préféré des insectes ?"
+ *                 answer:
+ *                   type: string
+ *                   example: "Le cricket"
+ *       "500":
+ *         description: Erreur interne
+ */
+router.post("/api/v1/jokes", BlagueController.createNewJoke);
 
 
 
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const dotenv = require("dotenv"); // pour lire les variables d'environnement du 
 const cors = require("cors"); // pour autoruiser toutes les origines en developpement
 const morgan = require("morgan"); // Pour logguer les requêtes dans le terminal
 const sequelize = require ("./app/db"); // on appelle l'instance sequelize 
-
+const swaggerUi = require('swagger-ui-express'); // middleware express qui sert l'interface visuelle Swagger UI à une route
+const swaggerJsdoc = require('swagger-jsdoc'); //c'est ce qui génère automatiquement le JSON openAPI à partir des commentaires dans les fichiers .js avec les @swagger au dessus des routes 
+const path = require('path'); 
 
 console.log("serveur qui démarre"); 
 
@@ -20,6 +22,25 @@ const PORT = process.env.PORT;
 
 // Initialisation de l'application Express : 
 const app = express(); 
+
+// Paramètres de configuration pour Swagger : 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: { // apparaitre en haut de la doc Swagger dans l'interface web 
+      title: 'API de mon projet',
+      version: '1.0.0',
+      description: 'Documentation de l’API avec Swagger',
+    },
+  },
+  apis: [path.join(__dirname, "app", "router.js")], 
+};
+
+// ici on génère la doc Swagger au format JSON à partir des options + des commentaires trouvés dans tes fichiers routes : 
+const swaggerSpec = swaggerJsdoc(options); 
+
+// Route Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 // Pour autoriser toutes les origines (domaines) des requests, à restreindre en prod :
