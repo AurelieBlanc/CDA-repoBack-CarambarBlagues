9e6d814a8d2fecfab5b863ac0af45f07709087e9
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-07-17 15:00:36 +0200

    modf swagger

--- app/router.js
+++ app/router.js
@@ -83,13 +83,21 @@ router.get("/api/v1/jokes/random", BlagueController.getOneJokeRandomly);
 // Route pour récupérer une seule blague par ID : 
 /**
  * @swagger
- * /api/v1/jokes/:id:
+ * /api/v1/jokes/{id}:
  *   get:
  *     summary: Récupère une blague par son ID (identifiant)
  *     tags:
  *       - Blagues
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: L'identifiant de la blague à récupérer
+ *         schema:
+ *           type: integer
+ *           example: 1
  *     responses:
- *       "200":
+ *       200:
  *         description: Succès
  *         content:
  *           application/json:
@@ -105,9 +113,9 @@ router.get("/api/v1/jokes/random", BlagueController.getOneJokeRandomly);
  *                 answer:
  *                   type: string
  *                   example: "L'Amsterdam"
- *       "404":
+ *       404:
  *         description: Aucune blague n'a été trouvée
- *       "500":
+ *       500:
  *         description: Erreur interne
  */
 router.get("/api/v1/jokes/:id", BlagueController.getOneJoke);
@@ -124,19 +132,23 @@ router.get("/api/v1/jokes/:id", BlagueController.getOneJoke);
  *       - Blagues
  *     requestBody:
  *       required: true
+ *       description: Objet JSON contenant la question et la réponse de la blague
  *       content:
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - question
+ *               - answer
  *             properties:
  *               question:
  *                 type: string
- *                 example: "Quel est le sport préféré des insectes ?"
+ *                 example: "Que dit un oignon quand il se cogne ?"
  *               answer:
  *                 type: string
- *                 example: "Le cricket"
+ *                 example: "Aïe"
  *     responses:
- *       "200":
+ *       200:
  *         description: Blague ajoutée avec succès
  *         content:
  *           application/json:
@@ -148,11 +160,11 @@ router.get("/api/v1/jokes/:id", BlagueController.getOneJoke);
  *                   example: 42
  *                 question:
  *                   type: string
- *                   example: "Quel est le sport préféré des insectes ?"
+ *                   example: "Que dit un oignon quand il se cogne ?"
  *                 answer:
  *                   type: string
- *                   example: "Le cricket"
- *       "500":
+ *                   example: "Aïe"
+ *       500:
  *         description: Erreur interne
  */
 router.post("/api/v1/jokes", BlagueController.createNewJoke);
